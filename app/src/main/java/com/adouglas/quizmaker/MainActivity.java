23d47ad3f3ce82e0e99963fb76329091cf80dcb7
deadmaus <andrew.douglas758@gmail.com>
2016-10-01 11:11:03 +1000

    Main and test activity

--- app/src/main/java/com/adouglas/quizmaker/MainActivity.java
+++ app/src/main/java/com/adouglas/quizmaker/MainActivity.java
@@ -1,13 +1,82 @@
 package com.adouglas.quizmaker;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void OnTakeTest(View view) {
+        Toast.makeText(getApplicationContext(), "on take test", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, testsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMakeTest(View view)
+    {
+        Toast.makeText(getApplicationContext(), "on make test", Toast.LENGTH_LONG).show();
+    }
+
+    public void onTestResults(View view)
+    {
+        Toast.makeText(getApplicationContext(), "on test results", Toast.LENGTH_LONG).show();
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/adouglas/quizmaker/testsActivity.java
@@ -0,0 +1,13 @@
+package com.adouglas.quizmaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tests);
+    }
+}
