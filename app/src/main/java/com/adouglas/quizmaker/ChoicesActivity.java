e5667796282ce896e2ca6b78675b87be9b8310c0
Andrew Douglas <andrew.douglas758@gmail.com>
2016-10-03 14:45:34 +1100

    Add event handlers for choices list activity.

--- app/src/main/java/com/adouglas/quizmaker/ChoicesActivity.java
+++ app/src/main/java/com/adouglas/quizmaker/ChoicesActivity.java
@@ -1,13 +1,36 @@
 package com.adouglas.quizmaker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChoicesActivity extends Activity {
+
+    private Question question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choices);
+
+        Intent intent = getIntent();
+        String questionId = intent.getStringExtra("question_id");
+
+        question = Question.findById(Question.class, Long.parseLong(questionId));
+    }
+
+    public void onQuestion(View view)
+    {
+        Intent intent = new Intent(this, AddQuestionActivity.class);
+        intent.putExtra("question_id", question.getId().toString());
+        startActivity(intent);
+    }
+
+    public void onAddChoice(View view)
+    {
+        Intent intent = new Intent(this, AddChoiceActivity.class);
+        intent.putExtra("question_id", question.getId().toString());
+        startActivity(intent);
     }
 }
