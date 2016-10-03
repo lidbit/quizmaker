554102927deb69f76886051b141cc109f523d210
Andrew Douglas <andrew.douglas758@gmail.com>
2016-10-04 00:46:16 +1100

    Test results model classes.

---
+++ app/src/main/java/com/adouglas/quizmaker/QuestionResult.java
@@ -0,0 +1,18 @@
+package com.adouglas.quizmaker;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by andrew on 4/10/2016.
+ */
+
+public class QuestionResult extends SugarRecord {
+    String questionContent;
+    boolean correct;
+    String userChoice;
+    String correctChoice;
+
+    public QuestionResult() {}
+
+    TestResult testResult;
+}
---
+++ app/src/main/java/com/adouglas/quizmaker/TestResult.java
@@ -0,0 +1,25 @@
+package com.adouglas.quizmaker;
+
+import com.orm.SugarRecord;
+import java.util.Date;
+
+/**
+ * Created by andrew on 4/10/2016.
+ */
+
+public class TestResult extends SugarRecord {
+    String testName;
+    String testDescription;
+    Date dateTaken;
+    int score;
+    int totalQuestions;
+    int testId;
+
+    public TestResult() {}
+
+    @Override
+    public String toString()
+    {
+        return testName;
+    }
+}
