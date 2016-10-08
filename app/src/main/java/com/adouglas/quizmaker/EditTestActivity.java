4aa97a143877d90567bb05c418cdddcbd881eace
Andrew Douglas <andrew.douglas758@gmail.com>
2016-10-08 22:06:01 +1100

    Remove save cancel on the edit test, data saved on each focus change if different.

--- app/src/main/java/com/adouglas/quizmaker/EditTestActivity.java
+++ app/src/main/java/com/adouglas/quizmaker/EditTestActivity.java
@@ -1,14 +1,30 @@
 package com.adouglas.quizmaker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EditTestActivity extends Activity {
 
     Test test;
+    List<Question> questions;
+    Map<Question, List<Choice>> questionListMap;
+    ExpandableListView expListView;
+    private ExpandableListAdapter expListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +39,175 @@ public class EditTestActivity extends Activity {
             ((EditText) findViewById(R.id.txtTestName)).setText(test.name);
             ((EditText) findViewById(R.id.txtTestDescription)).setText(test.description);
             ((EditText) findViewById(R.id.txtTestTimelimit)).setText(test.timelimit);
+
+            questions = Question.find(Question.class, "test = ?", testId);
         }
         else
         {
+            // We can always delete a test and questions
+            // lets not bother with save/cancel buttons
             test = new Test();
+            test.name = "test name";
+            test.description = "test description";
+            test.timelimit = "60";
+            test.save();
+            questions = new ArrayList<>();
+            Question q = new Question();
+            q.content = "sample question";
+            q.test = test;
+            q.save();
+            questions.add(q);
+            questionListMap = new LinkedHashMap<>();
         }
+
+        questionListMap = new LinkedHashMap<>();
+        for (Question q: questions) {
+            questionListMap.put(q, q.getChoices());
+        }
+        expListView = (ExpandableListView) findViewById(R.id.question_list);
+        expListAdapter = new ExpandableListAdapter(
+                this, questions, questionListMap);
+
+        expListView.setAdapter(expListAdapter);
+
+        registerForContextMenu(expListView);
+        expListView.setLongClickable(true);
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                final Choice choice = (Choice) expListAdapter.getChild(
+                        groupPosition, childPosition);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(EditTestActivity.this);
+                alertDialog.setTitle("Add Choice");
+                alertDialog.setMessage("Enter Question Content");
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), choice.choiceContent, Toast.LENGTH_SHORT);
+                        dialog.cancel();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Add Choice", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), choice.choiceContent, Toast.LENGTH_SHORT);
+                        dialog.cancel();
+                    }
+                });
+
+                alertDialog.show();
+
+                return true;
+            }
+        });
+
+        final EditText testName = (EditText)findViewById(R.id.txtTestName);
+        testName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    String testNameValue = testName.getText().toString();
+
+                    if(testNameValue != null && test.name != null
+                            && !testNameValue.isEmpty()
+                            && !test.name.equals(testNameValue))
+                    {
+                        test.name = testNameValue;
+                        test.save();
+                        Toast.makeText(getApplicationContext(), "Test updated", Toast.LENGTH_SHORT);
+                    }
+                }
+            }
+        });
+        final EditText testDesc = (EditText) findViewById(R.id.txtTestDescription);
+        testDesc.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    String testDescValue = testDesc.getText().toString();
+                    if(testDescValue != null && test.description != null
+                            && !testDescValue.isEmpty()
+                            && !test.description.equals(testDescValue))
+                    {
+                        test.description = testDesc.getText().toString();
+                        test.save();
+                        Toast.makeText(getApplicationContext(), "Test updated", Toast.LENGTH_SHORT);
+                    }
+                }
+            }
+        });
+        final EditText testTime = (EditText) findViewById(R.id.txtTestTimelimit);
+        testTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    String testTimeValue = testTime.getText().toString();
+                    if(testTimeValue != null && test.timelimit != null
+                        && !testTimeValue.isEmpty()
+                        && !test.timelimit.equals(testTimeValue))
+                    {
+                        test.timelimit = testTime.getText().toString();
+                        test.save();
+                        Toast.makeText(getApplicationContext(), "Test updated", Toast.LENGTH_SHORT);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        menu.add(Menu.NONE, 1, Menu.NONE, "Edit");
+        menu.add(Menu.NONE, 2, Menu.NONE, "delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem menuItem)
+    {
+        ExpandableListView.ExpandableListContextMenuInfo info =
+                (ExpandableListView.ExpandableListContextMenuInfo) menuItem.getMenuInfo();
+        switch (menuItem.getItemId())
+        {
+            case 1:
+                editQuestion(info.id);
+                return true;
+            case 2:
+                deleteQuestion(info.id);
+                return true;
+            default:
+                return super.onContextItemSelected(menuItem);
+        }
+    }
+
+    public void editQuestion(long id)
+    {
+        Question question = questions.get((int)id);
+        Intent intent = new Intent(this, AddQuestionActivity.class);
+        intent.putExtra("question_id", question.getId().toString());
+        startActivity(intent);
+    }
+
+    public void deleteQuestion(long id)
+    {
+        Question question = questions.get((int)id);
+        List<Choice> choices = Choice.find(Choice.class, "question = ?", question.getId().toString());
+        if(choices != null && choices.size() > 0)
+        {
+            for(int i = 0; i < choices.size(); i++)
+            {
+                choices.get(i).delete();
+            }
+        }
+        questions.remove((int)id);
+        question.delete();
+        expListAdapter.notifyDataSetChanged();
     }
 
     public void onSave(View view)
